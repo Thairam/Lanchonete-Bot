7a15b399b17a04dc1bf64e4016a780fd994f0379
Adson Macedo <agnsoft@hotmail.com>
2019-11-17 23:02:27 -0300

    iniciando os trabalhos

--- src/parte-4/lanchoneteApp.js
+++ src/parte-4/lanchoneteApp.js
@@ -4,11 +4,24 @@ require('dotenv-extended').load({
     path: '../../.env'
 });
 
+const pedido = {}
+
 const moment = require("moment");
 const builder = require("botbuilder");
 const restify = require("restify");
 
 const server = restify.createServer();
+
+var vendas = {
+    "Tapioca": 0,
+    "Pizza": 0,
+    "Suco": 0,
+    "Batata Frita": 0,
+    "Hamburger": 0,
+    "X-Burger": 0,
+    "Pastel": 0,
+    "Sobremesa": 0
+};
 
 //===> Configuração do Bot:
 let connector = new builder.ChatConnector({
@@ -23,6 +36,17 @@ let recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
 let intents = new builder.IntentDialog({ recognizers: [recognizer] });
 
 //===> Configuração dos 'Intents'(Intenções):
+
+//Endpoint - Saudar:
+intents.matches("Reclamações", [ 
+    session => {
+        builder.Prompts.text(session, "Ok, qual seria a reclamação?");
+    },
+
+    (session, results) => {
+        session.send("Sua reclamação foi registrada com sucesso, posso ajudar em algo mais?")
+    }
+]);
 
 //Endpoint - Saudar:
 intents.matches("Saudar", (session, results) => {
@@ -42,6 +66,7 @@ intents.matches("Pedir", [
             "Pastel",
             "Sobremesa"
         ];
+
         let entityLanche = builder.EntityRecognizer.findEntity(args.entities, "Lanche");
 
         //Aqui estaremos verificando com o LUIS os melhores 'matches' para a solicitação
@@ -52,20 +77,37 @@ intents.matches("Pedir", [
 
         //Caso não encontre o que o usuário está solicitando:
         if (!match) {
-            builder.Prompts.choice(session, "No momento só temos esses lanches disponíveis. Qual que você gostaria de pedir?", lanches);
+            const bestSeller = Object.entries(vendas).reduce((e1, e2) => e1[1] > e2[1] ? e1 : e2);
+
+            builder.Prompts.choice(session, "No momento só temos esses lanches disponíveis. Qual que você gostaria de pedir?", lanches, {
+                retryPrompt: 'Escolha uma das opções abaixo. Uma sugestão seria **' + bestSeller[0] + '**' ,
+                maxRetries: 5
+            });
         } else {
             next({ response: match });
         }
     },
+    
+    (session, results) =>{
+        pedido.item = results.response.entity;
+        builder.Prompts.text(session, "Qual o endereco de entrega?")
+    }, 
+    
     (session, results) => {
+        pedido.endereco = results.response;
         //Aqui é para indicar em quanto tempo o pedido do lanche deverá ser entregue: em 30 minutos:
+        
         if (results.response) {
             var time = moment().add(30, "m");
-
+            
             session.dialogData.time = time.format("HH:mm");
-            session.send("Pronto! Seu lanche **%s** chegará às **%s**.", results.response.entity, session.dialogData.time);
+            session.send("Pronto! Seu lanche **%s** chegará às **%s**.", pedido.item, session.dialogData.time);
+            session.send('O endereço de entrega é: ' + pedido.endereco)
+            session.send('**Obrigado pela preferência!!!**')
+
+            vendas[pedido.item]++;
         } else {
-            session.send("Sem problemas! Se não gostarem, podem pedir numa próxima vez! :D");
+            session.send("Sem problemas! Pode pedir numa próxima vez! :D");
         }
     }
 ]);
